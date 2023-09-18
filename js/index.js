aa9417beab69644cb31f17301f3857acad66d4ca
gnwll <124819613+gnwll@users.noreply.github.com>
2023-09-18 16:19:20 -0500

    js image fade on hover

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-$()
-console.log("Help Me.")
+const questlineImage = document.getElementById('questline-image');
+const vfwImage = document.getElementById('vfw-image');
+
+function addImageFadeEffect(imageElement) {
+    imageElement.addEventListener('mouseover', () => {
+        imageElement.style.opacity = '0.7';
+    });
+
+    imageElement.addEventListener('mouseout', () => {
+        imageElement.style.opacity = '1';
+    });
+}
+
+addImageFadeEffect(questlineImage);
+addImageFadeEffect(vfwImage);
